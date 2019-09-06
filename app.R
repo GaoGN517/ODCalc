cfefe73aa0a5b4e780d891f804d71bdabf7546a2
luyeqing517 <luyeqing517@gmail.com>
2019-09-05 23:22:42 -0500

    Edit headline

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("OD calculation"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
